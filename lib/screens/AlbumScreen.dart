d01d16514fb15a17dc11ff526bcfc9ee821c4506
Ananthakumar <ananthakumar.akr@gmail.com>
2024-11-27 01:00:29 +0530

    persist the scroll position for album horizontal listview

--- lib/screens/AlbumScreen.dart
+++ lib/screens/AlbumScreen.dart
@@ -13,6 +13,8 @@ class AlbumScreen extends StatefulWidget {
 }
 
 class _AlbumScreenState extends State<AlbumScreen> {
+  final PageStorageBucket _bucket = PageStorageBucket();
+
   @override
   void initState() {
     super.initState();
@@ -21,83 +23,88 @@ class _AlbumScreenState extends State<AlbumScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Albums"),
-        actions: [
-          TextButton(
-              onPressed: () {
-                context.read<AlbumBloc>().add(ClearCache());
-                context.read<AlbumBloc>().add(FetchAlbums());
-              },
-              child: const Text("Clear cache"))
-        ],
-      ),
-      body: BlocBuilder<AlbumBloc, AlbumState>(
-        builder: (context, state) {
-          if (state is AlbumLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is AlbumLoaded) {
-            return ListView.builder(
-              itemCount: state.albums.length * 1000,
-              itemBuilder: (context, index) {
-                // get the cyclic index
-                final cyclicAlbumIndex = index % state.albums.length;
-                final album = state.albums[cyclicAlbumIndex];
-                final photos = state.albumPhotos[album.id] ?? [];
+    return PageStorage(
+        bucket: _bucket,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Albums"),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    context.read<AlbumBloc>().add(ClearCache());
+                    context.read<AlbumBloc>().add(FetchAlbums());
+                  },
+                  child: const Text("Clear cache"))
+            ],
+          ),
+          body: BlocBuilder<AlbumBloc, AlbumState>(
+            builder: (context, state) {
+              if (state is AlbumLoading) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (state is AlbumLoaded) {
+                return ListView.builder(
+                  itemCount: state.albums.length * 1000,
+                  itemBuilder: (context, index) {
+                    // get the cyclic index
+                    final cyclicAlbumIndex = index % state.albums.length;
+                    final album = state.albums[cyclicAlbumIndex];
+                    final photos = state.albumPhotos[album.id] ?? [];
 
-                return Column(
-                  children: [
-                    Row(
+                    return Column(
                       children: [
-                        Container(
-                            padding: const EdgeInsets.all(8),
-                            child: Text(
-                              album.title,
-                              textAlign: TextAlign.right,
-                              style: const TextStyle(
-                                  fontSize: 16,
-                                  overflow: TextOverflow.ellipsis),
-                            )),
+                        Row(
+                          children: [
+                            Container(
+                                padding: const EdgeInsets.all(8),
+                                child: Text(
+                                  album.title,
+                                  textAlign: TextAlign.right,
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      overflow: TextOverflow.ellipsis),
+                                )),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 150,
+                          child: ListView.builder(
+                            key: PageStorageKey('album_$index'),
+                            scrollDirection: Axis.horizontal,
+                            itemCount: photos.length * 5000,
+                            itemBuilder: (context, index) {
+                              final cyclicPhotosIndex = index % photos.length;
+                              return Container(
+                                  padding: const EdgeInsets.all(4),
+                                  child: CachedNetworkImage(
+                                    imageUrl: photos[cyclicPhotosIndex].url,
+                                    placeholder: (context, url) =>
+                                        const SizedBox(
+                                            height: 150,
+                                            width: 150,
+                                            child: Center(
+                                                child:
+                                                    CircularProgressIndicator(
+                                              strokeWidth: 2,
+                                            ))),
+                                    errorWidget: (context, url, error) =>
+                                        const SizedBox(
+                                            height: 150,
+                                            width: 150,
+                                            child: Icon(Icons.error)),
+                                  ));
+                            },
+                          ),
+                        ),
+                        const Padding(padding: EdgeInsets.only(bottom: 16))
                       ],
-                    ),
-                    SizedBox(
-                      height: 150,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: photos.length * 5000,
-                        itemBuilder: (context, index) {
-                          final cyclicPhotosIndex = index % photos.length;
-                          return Container(
-                              padding: const EdgeInsets.all(4),
-                              child: CachedNetworkImage(
-                                imageUrl: photos[cyclicPhotosIndex].url,
-                                placeholder: (context, url) => const SizedBox(
-                                    height: 150,
-                                    width: 150,
-                                    child: Center(
-                                        child: CircularProgressIndicator(
-                                      strokeWidth: 2,
-                                    ))),
-                                errorWidget: (context, url, error) =>
-                                    const SizedBox(
-                                        height: 150,
-                                        width: 150,
-                                        child: Icon(Icons.error)),
-                              ));
-                        },
-                      ),
-                    ),
-                    const Padding(padding: EdgeInsets.only(bottom: 16))
-                  ],
+                    );
+                  },
                 );
-              },
-            );
-          } else {
-            return const Center(child: Text("Error loading albums"));
-          }
-        },
-      ),
-    );
+              } else {
+                return const Center(child: Text("Error loading albums"));
+              }
+            },
+          ),
+        ));
   }
 }
